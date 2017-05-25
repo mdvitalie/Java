1bb2d74714fb3743c104477041f3e3aef38185af
vitalie.ie <mdvitalie@gmail.com>
2017-05-25 15:42:45 +0100

    new file

--- sheet8BankAccount/BankAccount.java
+++ sheet8BankAccount/BankAccount.java
@@ -2,21 +2,143 @@ package sheet8BankAccount;
 
 public class BankAccount {
 
-	private String OnereName;
+	//Member variables
+	private String name;
 	private String address;
 	private int accountNumber;
 	private double balance;
 	
-	public static double interestRate;
+	//Static /class variables
+	public static final double INTREST_RATE =5;
 	public static int numOfBankAcounts;
-	public static int newAccountNumber;
+	public static int newAccountNumberCounter = 50008000;
 	
 	
-	public BankAccount() {
-		interestRate++;
+	private BankAccount() {
 		numOfBankAcounts++;
-		newAccountNumber++;
+		accountNumber = newAccountNumberCounter++;
 	}
+
+	
+
+	public BankAccount(String name, String address) {
+		this();
+		this.name = name;
+		this.address = address;
+	}
+
+
+
+	public BankAccount(String name, String address, double balance) {
+		this();
+		setName(name);
+		this.address = address;
+		//setAccountNumber(accountNumber);
+		this.balance = balance;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		
+		String regex= "[a-zA-z]+";
+		if(name.matches(regex)){
+			System.out.println("Invaled name");
+		}
+		else{
+			this.name = name;
+		}
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+
+	/*public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}*/
+
+
+	public double getBalance() {
+		return balance;
+	}
+
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+//Methods
+	public void depositMoney(double deposit){
+		if(deposit > 0){
+		     balance += deposit;
+		System.out.println(" Lodgement: "+ deposit);
+		}
+		else{
+		System.out.println( deposit+"can not be negative");
+		}
+	}
+	
+	public void withdrawMoney(double withdraw){
+		
+		if(withdraw> balance){
+			System.out.println(" No sufficient money in your account");
+			System.out.println(" The amount you can withdraw: " +balance);
+			
+		}else{
+			
+			this.balance -= withdraw;
+			System.out.println(" Withdraw: " + withdraw);
+		}
+	}
+	
+	public void withdrawAll(){
+		
+		System.out.printf("About to withdraw all, you have a balance of €%.2f\n",balance);
+		
+	}
+	
+	public void checkBalance(){
+		setBalance(balance);
+		System.out.println(" Total balance: " + balance);
+	}
+	
+	public double addInterest(){
+//		balance *= intrest;
+		balance = balance + (INTREST_RATE/100);
+		System.out.println( " Intrest of "+INTREST_RATE+"% :" + balance);
+
+		return balance;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return  "\n Name: \t" + name +
+				"\n Address: " + address +
+				"\n Account number: " + accountNumber +
+				"\n Balance: "
+				+ balance;
+	}
+	
 	
 	
 	
--- sheet8BankAccount/BankAccountMain.java
+++ sheet8BankAccount/BankAccountMain.java
@@ -3,7 +3,24 @@ package sheet8BankAccount;
 public class BankAccountMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		BankAccount bank = new BankAccount(" Vova Volchenko","Ireland Dublin 21 avenue",45000);
+		System.out.println(bank);
+		
+		bank.withdrawMoney(5000);
+		bank.addInterest();
+		
+		System.out.println(bank);
+		
+		bank.depositMoney(200);
+		System.out.println(bank);
+		
+		bank.withdrawMoney(50);
+		System.out.println(bank);
+		
+		bank.checkBalance();
+		System.out.println("#########################");
+		System.out.println(bank);
 
 	}
 
