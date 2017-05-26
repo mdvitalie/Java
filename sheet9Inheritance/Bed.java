529be71840882abe7976b8ee525eb9ba74eded5c
vitalie.ie <mdvitalie@gmail.com>
2017-05-26 12:26:09 +0100

    new update

--- sheet9Inheritance/Bed.java
+++ sheet9Inheritance/Bed.java
@@ -2,4 +2,65 @@ package sheet9Inheritance;
 
 public class Bed extends Furniture {
 
+	//member variable
+	private boolean hasHeadboard;
+	private String size;
+	
+	
+	 Bed() {
+		 
+	}
+
+
+	public Bed(String colour, String materialType,boolean hasHeadboard, String size) {
+		this();
+		this.hasHeadboard = hasHeadboard;
+		this.size = size;
+	}
+
+
+	/**
+	 * @return the hasHeadboard
+	 */
+	public boolean isHasHeadboard() {
+		return hasHeadboard;
+	}
+
+
+	/**
+	 * @param hasHeadboard the hasHeadboard to set
+	 */
+	public void setHasHeadboard(boolean hasHeadboard) {
+		this.hasHeadboard = hasHeadboard;
+	}
+
+
+	/**
+	 * @return the size
+	 */
+	public String getSize() {
+		return size;
+	}
+
+
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "\n Has Head board: " + hasHeadboard + 
+				"\n Size=" + size;
+	}
+	
+	
+	
+	
 }
--- sheet9Inheritance/Chair.java
+++ sheet9Inheritance/Chair.java
@@ -2,4 +2,57 @@ package sheet9Inheritance;
 
 public class Chair extends Furniture {
 
+	//member variable
+	private boolean hasGaslift;
+	private String type;
+	
+	public Chair(){
+		
+	}
+	
+	public Chair(String colour, String materialType,boolean hasGaslift,String type){
+		this();
+		this.hasGaslift = hasGaslift;
+		this.type = type;
+		
+	}
+
+	/**
+	 * @return the hasGaslift
+	 */
+	public boolean isHasGaslift() {
+		return hasGaslift;
+	}
+
+	/**
+	 * @param hasGaslift the hasGaslift to set
+	 */
+	public void setHasGaslift(boolean hasGaslift) {
+		this.hasGaslift = hasGaslift;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Chair has gas lift: " + hasGaslift + 
+				"\n type=" + type;
+	}
+	
+	
 }
