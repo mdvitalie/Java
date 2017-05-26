7e9b217d0f15dfd31f3e35f31bf876391258cf16
vitalie.ie <mdvitalie@gmail.com>
2017-05-26 12:09:45 +0100

    new update

--- sheet9Inheritance/Furniture.java
+++ sheet9Inheritance/Furniture.java
@@ -2,4 +2,65 @@ package sheet9Inheritance;
 
 public class Furniture {
 	//member variable 
+	private String colour;
+	private String materialType;
+	
+	//Static / class variables
+	
+	public static int furnitureCounter;
+	public Furniture() {
+		furnitureCounter++;
+	}
+
+
+	public Furniture(String colour, String materialType) {
+		this();
+		this.colour = colour;
+		this.materialType = materialType;
+	}
+
+
+	/**
+	 * @return the colour
+	 */
+	public String getColour() {
+		return colour;
+	}
+
+
+	/**
+	 * @param colour the colour to set
+	 */
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+
+	/**
+	 * @return the materialType
+	 */
+	public String getMaterialType() {
+		return materialType;
+	}
+
+
+	/**
+	 * @param materialType the materialType to set
+	 */
+	public void setMaterialType(String materialType) {
+		this.materialType = materialType;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Colour: " + colour + 
+				"\n Material type: " + materialType;
+	}
+	
+	
+	
 }
--- sheet9Inheritance/Table.java
+++ sheet9Inheritance/Table.java
@@ -2,4 +2,58 @@ package sheet9Inheritance;
 
 public class Table extends Furniture {
 
+	//member variable
+	private boolean isExpandable;
+	private String shape;
+	
+	public Table() {
+
+	}
+
+	public Table(String colour, String materialType,boolean isExpandable, String shape) {
+		this();
+		this.isExpandable = isExpandable;
+		this.shape = shape;
+	}
+
+	/**
+	 * @return the isExpandable
+	 */
+	public boolean isExpandable() {
+		return isExpandable;
+	}
+
+	/**
+	 * @param isExpandable the isExpandable to set
+	 */
+	public void setExpandable(boolean isExpandable) {
+		this.isExpandable = isExpandable;
+	}
+
+	/**
+	 * @return the shape
+	 */
+	public String getShape() {
+		return shape;
+	}
+
+	/**
+	 * @param shape the shape to set
+	 */
+	public void setShape(String shape) {
+		this.shape = shape;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "\nTable: " + isExpandable +
+				"\n Shape: " + shape;
+	}
+	
+	
+	
+	
 }
