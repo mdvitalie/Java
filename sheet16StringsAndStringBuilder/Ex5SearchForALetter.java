4adf91f84a858474b176789c256486977eef618c
vitalie.ie <mdvitalie@gmail.com>
2017-06-12 17:08:11 +0100

    new update

--- sheet16StringsAndStringBuilder/Ex5SearchForALetter.java
+++ sheet16StringsAndStringBuilder/Ex5SearchForALetter.java
@@ -11,26 +11,28 @@ public class Ex5SearchForALetter {
 		Scanner scanner =new Scanner(System.in);
 
 		System.out.print("Type a word : ");
-		String usInput = scanner.nextLine();
+		String userInput = scanner.nextLine();
 
-		ex5.searchforLetter(usInput);
+		ex5.searchforLetter(userInput);
 		//System.out.println(chreturn);
-
+		scanner.close();
 	}
 
 	//Method
 	public void searchforLetter(String str){
 		Scanner scanner =new Scanner(System.in);
+
+
 		System.out.print("Search for a charater :");
-
-
-		String searchChar =scanner.nextLine();
-		char [] letters = searchChar.toCharArray();
 		
 
-		for(int i =0; i<letters.length; i++){
+		for(int i =0; i<str.length(); i++){
+			String searchChar =scanner.nextLine();
 			
-			if(str.contains(searchChar)){
+			char [] letters = searchChar.toCharArray();
+			
+			//if(str.contains(searchChar)){
+				if(str.contains(searchChar)){
 				int position = str.indexOf(searchChar);
 			
 				System.out.println("[ "+searchChar +" ] was found at position : " +( i+ (position +1)));
